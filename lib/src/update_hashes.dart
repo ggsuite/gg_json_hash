9be7630ae44dc6a066fa9464f9eaad9d969e9458
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2025-05-17 19:38:41 +0200

    Handle multiple objects with the same hash

--- lib/src/update_hashes.dart
+++ lib/src/update_hashes.dart
@@ -20,11 +20,101 @@ class UpdateHashes {
     _init();
   }
 
+  /// The original objects by hashes
+  Map<String, dynamic> get originalObjects => jsonInfo.hashToObjects;
+
+  /// The updated objects
+  Map<String, dynamic> updatedObjects = {};
+
+  /// Old to new hashes map
+  Map<String, String> oldToNewHashes = {};
+
+  /// The update JSON
+  late Map<String, dynamic> updatedJson;
+
   // ######################
   // Private
   // ######################
 
   void _init() {
+    jsonInfo.throwOnAmbigiousHashes();
+    jsonInfo.throwOnCircularDependencies();
+    _initUpdatedObjects();
+    _initUpdatedJson();
+  }
+
+  void _initUpdatedObjects() {
     final updateOrder = jsonInfo.updateOrder;
+    for (final hash in updateOrder) {
+      // Get the object of the hash
+      final object = jsonInfo.hashToObjects[hash]!;
+
+      // Remember the old hash
+      final oldHash = object['_hash'] as String;
+
+      // Reset the hash
+      object['_hash'] = '';
+
+      // Replace references to other hashes
+      _translateReferences(object);
+
+      // Update the hash
+      hip(object, updateExistingHashes: false, throwIfWrongHashes: false);
+
+      // Remember the old to new hash
+      final newHash = object['_hash'] as String;
+
+      if (newHash != oldHash) {
+        oldToNewHashes[oldHash] = newHash;
+      }
+
+      updatedObjects[newHash] = object;
+    }
+  }
+
+  void _translateReferences(Map<String, dynamic> object) {
+    for (final entry in object.entries) {
+      final key = entry.key;
+      final value = entry.value;
+
+      // Translate the key
+      final translatedKey = _translate(key) as String;
+
+      // Translate the value
+      final translatedValue = _translate(value);
+
+      // Remove the old key if it is different from the new key
+      if (translatedKey != key) {
+        object.remove(key);
+      }
+      object[translatedKey] = translatedValue;
+    }
+  }
+
+  dynamic _translate(dynamic val) {
+    if (val is String) {
+      for (final entry in oldToNewHashes.entries) {
+        final oldHash = entry.key;
+        final newHash = entry.value;
+        val = val.replaceAll(oldHash, newHash);
+      }
+    } else if (val is List) {
+      final copy = [...val];
+      for (var i = 0; i < val.length; i++) {
+        copy[i] = _translate(val[i]);
+      }
+      val = copy;
+    }
+
+    return val;
+  }
+
+  void _initUpdatedJson() {
+    final oldRootHash = jsonInfo.updateOrder.last;
+    final newRootHash = oldToNewHashes[oldRootHash];
+    final newRoot = updatedObjects[newRootHash] as Map<String, dynamic>;
+    const jh = JsonHash.defaultInstance;
+    jh.validate(newRoot);
+    updatedJson = newRoot;
   }
 }
--- test/update_hashes_test.dart
+++ test/update_hashes_test.dart
@@ -45,6 +45,182 @@ void main() {
             ].join('\n'),
           );
         });
+
+        test('when json data contains ambiguous hashes', () {
+          final json = {
+            '_hash': 'root',
+            'child0': {
+              '_hash': 'CHILD',
+              'key': 'a',
+            },
+            'child1': {
+              '_hash': 'CHILD',
+              'key': 'b',
+            },
+          };
+
+          final updateHashes = UpdateHashes(json: json);
+          var message = '';
+          try {
+            updateHashes.apply();
+          } catch (e) {
+            message = e.toString();
+          }
+
+          expect(
+            message,
+            [
+              'Exception: Cannot update hashes: Ambigious hashes detected:',
+              '  - CHILD',
+            ].join('\n'),
+          );
+        });
+      });
+
+      group('updates hashes and references in an object', () {
+        test('that is empty', () {
+          const json = {
+            '_hash': 'ROOT',
+          };
+
+          final updateHashes = UpdateHashes(json: json);
+          updateHashes.apply();
+
+          final uj = updateHashes.updatedJson;
+
+          expect(uj, {'_hash': 'RBNvo1WzZ4oRRq0W9-hknp'});
+        });
+
+        test('with one child', () {
+          const json = {
+            '_hash': 'ROOT',
+            'child0': {
+              '_hash': 'CHILD0',
+            },
+          };
+
+          final updateHashes = UpdateHashes(json: json);
+          updateHashes.apply();
+
+          final uj = updateHashes.updatedJson;
+
+          expect(uj, {
+            '_hash': 'StnzK_1GZNY07Y0Ucuv3Sm',
+            'child0': {'_hash': 'RBNvo1WzZ4oRRq0W9-hknp'},
+          });
+        });
+
+        test('with a references to other objects', () {
+          const json = {
+            '_hash': 'ROOT',
+            'child0': {
+              '_hash': 'CHILD0',
+              'ref': 'CHILD1',
+            },
+            'child1': {
+              '_hash': 'CHILD1',
+              'ref': 'CHILD2',
+            },
+            'child2': {
+              '_hash': 'CHILD2',
+            },
+          };
+
+          final updateHashes = UpdateHashes(json: json);
+          updateHashes.apply();
+
+          final uj = updateHashes.updatedJson;
+
+          expect(uj, {
+            '_hash': 'oGPF9fMYO89rh97-Y5eVTI',
+            'child0': {
+              '_hash': '_ByJerk4dXxFMBxPZAapfW',
+              'ref': '3PY_gxJ7g-ZyVcwt1JiGLa',
+            },
+            'child1': {
+              '_hash': '3PY_gxJ7g-ZyVcwt1JiGLa',
+              'ref': 'RBNvo1WzZ4oRRq0W9-hknp',
+            },
+            'child2': {'_hash': 'RBNvo1WzZ4oRRq0W9-hknp'},
+          });
+        });
+
+        test('with nested arrays', () {
+          const json = {
+            '_hash': 'ROOT',
+            'child0': {
+              '_hash': 'CHILD0',
+              'ref': [
+                [
+                  {
+                    '_hash': 'CHILD1',
+                    'ref': [
+                      {
+                        '_hash': 'CHILD2',
+                      },
+                      'CHILD3',
+                    ],
+                  },
+                ],
+              ],
+            },
+            'child3': {
+              '_hash': 'CHILD3',
+              'key': 'VALUE',
+            },
+          };
+
+          final updateHashes = UpdateHashes(json: json);
+          updateHashes.apply();
+
+          final uj = updateHashes.updatedJson;
+
+          expect(uj, {
+            '_hash': 'JemEH8yDi5s4UkazrZhpwt',
+            'child0': {
+              '_hash': 'HG90sRuwNhlwRo8bqty8BF',
+              'ref': [
+                [
+                  {
+                    '_hash': 'G1fQls2aKWdRBynY-l069A',
+                    'ref': [
+                      {'_hash': 'RBNvo1WzZ4oRRq0W9-hknp'},
+                      'JczQWaAeuCdYpXBHjbUNr_',
+                    ],
+                  }
+                ]
+              ],
+            },
+            'child3': {'_hash': 'JczQWaAeuCdYpXBHjbUNr_', 'key': 'VALUE'},
+          });
+        });
+
+        test('with multiple identical child objects', () {
+          const json = {
+            '_hash': 'ROOT',
+            'array': [
+              {
+                'key': 'value',
+                '_hash': 'HASH',
+              },
+              {
+                'key': 'value',
+                '_hash': 'HASH',
+              },
+            ],
+          };
+
+          final updateHashes = UpdateHashes(json: json);
+          updateHashes.apply();
+
+          expect(updateHashes.updatedJson, {
+            '_hash': '_28DJCUI3zppsTIy8QjLUS',
+            'array': [
+              {'key': 'value', '_hash': '5Dq88zdSRIOcAS-WM_lYYt'},
+              {'key': 'value', '_hash': '5Dq88zdSRIOcAS-WM_lYYt'},
+            ],
+          });
+        });
       });
     });
   });
